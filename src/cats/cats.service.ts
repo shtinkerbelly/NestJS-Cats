b48c64d8e597051e172c8888e00c3ddddb3adf35
c672670 <c672670@persi-ubun22-10.CASA.NET>
2026-04-16 17:51:15 +0300

    feat: implement cats.service

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CatsService {
+  private readonly cats: Cat[] = [];
+
   create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    console.log(createCatDto);
+    const newCat: Cat = {
+      id: uuid(),
+      ...createCatDto,
+    };
+    this.cats.push(newCat);
+    return newCat;
   }
 
   findAll() {
-    return `This action returns all cats`;
+    return this.cats;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cat`;
+  findOne(id: string) {
+    return this.cats.find((cat) => cat.id === id);
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+  update(id: string, updateCatDto: UpdateCatDto) {
+    return this.cats.map((cat) =>
+      cat.id === id ? { ...cat, ...updateCatDto } : cat,
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cat`;
+  remove(id: string) {
+    return this.cats.filter((cat) => cat.id !== id);
   }
 }
